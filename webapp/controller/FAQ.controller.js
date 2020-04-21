8e0f476e2b5ce76df17ef1a46e84867e9f7abeb9
Ramsunder Nishkalen <nishkalen.ramsunder@britehouse.co.za>
2020-04-21 16:39:44 +0000

    Lockdown FAQ - V2.1 (AddedSolutions)

--- webapp/controller/FAQ.controller.js
+++ webapp/controller/FAQ.controller.js
@@ -5,7 +5,22 @@ sap.ui.define([
 
 	return Controller.extend("gdsd.FAQ_App.controller.FAQ", {
 		onInit: function () {
+		},
+		
+		
+formatterSolution: function (SolutionS) {
+    var array = {};
+    array = SolutionS.split('*');
+    
+    
+   var solution = [];
+    for(var i = 1; i<array.length; i++){
+    	solution += array[i] + "\n" ;	
+    }
+    
+ return solution;
+}
 
-		}
+
 	});
 });
